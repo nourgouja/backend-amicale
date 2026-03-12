bdbce44cfc4930893fe4ff67267e7bd200b1a939
nourgouja <164974981+nourgouja@users.noreply.github.com>
2026-03-12 22:07:16 +0100

    Revert "Offre"

--- Pfe/src/main/java/tn/star/Pfe/enums/StatutInscription.java
+++
@@ -1,8 +0,0 @@
-package tn.star.Pfe.enums;
-
-public enum StatutInscription {
-    EN_ATTENTE,
-    CONFIRMEE,
-    ANNULEE
-
-}
--- Pfe/src/main/java/tn/star/Pfe/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package tn.star.Pfe.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
